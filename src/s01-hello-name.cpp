ad6c5d45f8e6bb20234215f87b3e8cfe0c31cb02
Bartek <s24276@pjwstk.edu.pl>
2021-10-13 17:40:28 +0200

    wstep

---
+++ src/s01-hello-name.cpp
@@ -0,0 +1,19 @@
+/*
+ * Reading command line arguments.
+ */
+
+#include <iostream>
+#include <string>
+
+auto main() -> int 
+{
+	auto name = std::string{};
+	std::getline(std::cin, name);
+
+
+
+	std::cout <<"Hello, "<< name <<"!\n";
+
+
+	return 0;
+}
---
+++ src/s01-sub.cpp
@@ -0,0 +1,19 @@
+/*
+ * Reading command line arguments.
+ */
+
+#include <iostream>
+#include <string>
+
+auto main(int argc, char* argv[]) -> int 
+{
+	if (argc < 2) {
+	std::cout << "Podaj argumenty \n";
+	return 0;
+ }
+	auto const a = std::stoi(argv[1]);
+	auto const b = std::stoi(argv[2]);
+	
+	std::cout << (a - b) << "\n";
+	return 0;
+}
